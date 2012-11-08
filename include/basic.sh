0427feef0ac2fc0952ff59cec323d449883e27e7
Dominik D. Geyer <dominik.geyer@gmail.com>
2012-11-09 00:25:02 +0100

    pkgbox_echo(): Much simpler approach for modifying SGR parameters
    Embed the output of pkgbox_sgr() into strings by using command
    substitution.

--- include/basic.sh
+++ include/basic.sh
@@ -1,27 +1,44 @@
 # http://en.wikipedia.org/wiki/ANSI_escape_code#graphics
 # http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-function pkgbox_echo()
+function pkgbox_sgr()
 {
-	local magic=':' csi='\e[' term='m' arg color
+	local csi='\e[' term='m' arg sgr opt m c
 	
-	declare -A colors=( \
-		[black]=0  [red]=1   [green]=2    [yellow]=3 \
-		[blue]=4   [cyan]=6  [magenta]=5  [white]=7  \
-	)
-	
-	declare i=0
-	
-	for arg in "$@"; do
-		color="${arg:1:${#arg}-2}"  # NOTE: the construct "${arg:1:-1}" ony works in bash >= 4.2
+	for arg in $*; do
+		opt=${arg%%=*}
+		case $opt in
+		"bold")      m=1 ;;
+		"underline") m=4 ;;
+		"blink")     m=5 ;;
+		"reverse")   m=7 ;;
+		"fg" | "bg")
+			[[ $opt == "fg" ]] && m=3 || m=4
+			case ${arg#*=} in
+			"red")     c=1 ;;
+			"green")   c=2 ;;
+			"yellow")  c=3 ;;
+			"blue")    c=4 ;;
+			"magenta") c=5 ;;
+			"cyan")    c=6 ;;
+			"white")   c=7 ;;
+			*)         c=0;  # black and others
+			esac
+			m="${m}${c}"
+			;;
+		*) m=0 ;;  # reset/normal
+		esac
 		
-		if [[ $arg == ${magic}*${magic} && ${#color} > 0 && ${colors[$color]} ]]; then
-			echo -n -e ${csi}"0;3"${colors[$color]}${term}
-		else
-			(( i++ > 0  )) && echo -n " "
-			echo -n "$arg"
-		fi
+		sgr="${sgr}${m};"
 	done
 	
-	echo -e ${csi}0${term}  # reset SGR and break line
+	[[ -z "$sgr" ]] && sgr="0" || sgr="${sgr:0:${#sgr}-1}"	# remove trailing ";"
+		
+	#echo -n " { ${csi}${sgr}${term}] }"	#debug
+	echo -n -e "${csi}${sgr}${term}"
+}
+
+function pkgbox_echo()
+{
+	echo "$@$(pkgbox_sgr)"	# echo and reset SGR
 }
 
