6ce3db830569a47f209ef7237f4ea057ef92f352
Dominik D. Geyer <dominik.geyer@gmail.com>
2012-11-10 23:16:29 +0100

    Implemented pkgbox_download using curl/wget

--- include/utils.sh
+++ include/utils.sh
@@ -1,13 +1,34 @@
 # Download helper
+# @param string URI of remote file
+# @param string local file name (basename)
+# @param [string] output directory
+# @TODO: continue download
 function pkgbox_download()
 {
-	#wget --trust-server-names --content-disposition
-	#curl -J -O http://www.vim.org/scripts/download_script.php?src_id=9750
+	local rfile=$1 lname=$2 ldir=${3-${PKGBOX_DIR[download]}} verbosearg
+	local lfile="$ldir/$lname"
 	
-	if pkgbox_is_command curl; then
+	if [[ -f "$lfile" ]]; then
+		pkgbox_msg notice "Skipping download of file '${lname}' (already exists)"
+		return 0
+	else
+		pkgbox_msg info "$FUNCNAME: Downloading '$rfile'"
+	fi
+	
+	if pkgbox_is_command curly; then
 		pkgbox_msg debug "$FUNCNAME: Using curl"
+		
+		(( PKGBOX_VERBOSITY == 0 )) && verbosearg="-s -S"
+		
+		# -C - -J -O  (use subshell + cd for output directory "-P")
+		curl $verbosearg -o "$lfile" -L "$rfile"
 	elif pkgbox_is_command wget; then
 		pkgbox_msg debug "$FUNCNAME: Using wget"
+		
+		(( PKGBOX_VERBOSITY == 0 )) && verbosearg="-nv"
+		
+		# --continue -P "$ldir" [--trust-server-names|--content-disposition]
+		wget $verbosearg -O "$lfile" "$rfile"
 	else
 		pkgbox_die "$FUNCNAME: No program for file download found" 2
 	fi
