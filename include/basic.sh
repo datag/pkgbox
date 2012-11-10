56e00aa6d6489e31922154e3fb4141c3513161d5
Dominik D. Geyer <dominik.geyer@gmail.com>
2012-11-10 19:06:31 +0100

    pkgbox_sgr(): Added aliases and minor cleanups

--- include/basic.sh
+++ include/basic.sh
@@ -1,29 +1,33 @@
-# http://en.wikipedia.org/wiki/ANSI_escape_code#graphics
-# http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+# Set SGR (Select Graphic Rendition) parameters
+#
+# If called without parameters it will reset SGR
+#
+# @param [string...] Option
+# @url http://en.wikipedia.org/wiki/ANSI_escape_code#graphics
+# @url http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
 function pkgbox_sgr()
 {
-	local csi='\e[' term='m' arg sgr opt m c
+	local csi='\e[' term='m' arg sgr opt m
 	
 	for arg in $*; do
 		opt=${arg%%=*}
 		case $opt in
-		"bold")      m=1 ;;
-		"underline") m=4 ;;
-		"blink")     m=5 ;;
-		"reverse")   m=7 ;;
-		"fg" | "bg")
-			[[ $opt == "fg" ]] && m=3 || m=4
+		"bold" | "bright")      m=1 ;;
+		"underline")            m=4 ;;
+		"blink")                m=5 ;;
+		"reverse" | "inverse")  m=7 ;;
+		"fg" | "c" | "bg")
 			case ${arg#*=} in
-			"red")     c=1 ;;
-			"green")   c=2 ;;
-			"yellow")  c=3 ;;
-			"blue")    c=4 ;;
-			"magenta") c=5 ;;
-			"cyan")    c=6 ;;
-			"white")   c=7 ;;
-			*)         c=0 ;;  # black and others
+			"red")     m=1 ;;
+			"green")   m=2 ;;
+			"yellow")  m=3 ;;
+			"blue")    m=4 ;;
+			"magenta") m=5 ;;
+			"cyan")    m=6 ;;
+			"white")   m=7 ;;
+			*)         m=0 ;;  # black and others
 			esac
-			m="${m}${c}"
+			[[ $opt == "bg" ]] && m="4${m}" || m="3${m}"
 			;;
 		*) m=0 ;;  # reset/normal
 		esac
@@ -31,9 +35,8 @@ function pkgbox_sgr()
 		sgr="${sgr}${m};"
 	done
 	
-	[[ -z "$sgr" ]] && sgr="0" || sgr="${sgr:0:${#sgr}-1}"	# remove trailing ";"
-		
-	#echo -n " { ${csi}${sgr}${term}] }"	#debug
+	[[ -z "$sgr" ]] && sgr="0" || sgr="${sgr:0:${#sgr}-1}"	# remove superfluous trailing ";"
+	
 	echo -n -e "${csi}${sgr}${term}"
 }
 
