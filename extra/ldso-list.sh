5d1bce7a7322dfb3bdb9e16cf689fe6a313282d9
Dominik D. Geyer <dominik.geyer@gmail.com>
2013-03-05 21:23:39 +0100

    Added small script for verifying dynamically linked binaries/libraries

---
+++ extra/ldso-list.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+[[ $# == 1 ]] || { echo "Usage: ${0##*/} <path>" >&2; exit 1; }
+
+export LC_ALL=C
+
+SCANPATH=$1
+IPREFIX=${2-$SCANPATH}
+
+RTLDLIST="/lib/ld-linux.so.2 /lib64/ld-linux-x86-64.so.2"
+
+function parse_line() {
+	echo "Library: $@" >&2
+	
+	local lib
+	[[ $# == 4 ]] && lib=$3 || lib=$1
+	
+	libs+=( $lib )
+}
+
+declare -a libs
+
+while read -r f; do
+	for rtld in $RTLDLIST; do
+		[[ -x $rtld ]] || continue
+		
+		$rtld --verify "$f"
+		case $? in
+		[02])
+			: # success
+			;;
+		*)
+			# not an ELF or RTLD not suitable? skip it...
+			continue
+			;;
+		esac
+	
+		echo "Processing: $f" >&2
+	
+		while read -r l; do
+			parse_line $l
+		done < <($rtld --list "$f")
+		
+		# skip other RTLD
+		continue
+	done
+done < <(find "$SCANPATH" -type f -print)
+
+
+for lib in "${libs[@]}"; do
+	echo ${lib/$IPREFIX/"{_PREFIX}/"}
+done | sort | uniq -c
+
